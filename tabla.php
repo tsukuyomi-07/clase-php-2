2c1b37363e7205be9dc9a541fa981cf792992b77
tsukuyomi-07 <sebas.07d@gmail.com>
2024-12-10 18:14:43 -0500

    1

---
+++ tabla.php
@@ -0,0 +1,9 @@
+
+
+CREATE TABLE products (
+id BIGINT PRIMARY KEY AUTO_INCREMENT,
+product VARCHAR(30) NOT NULL,
+description VARCHAR (30) NULL,
+price INT (6) NOT NULL,
+status INT (1) DEFAULT (1)
+);
